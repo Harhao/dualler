15fd39d75a1adb3eb704bbbdee8d3e3d2119675f
harhao <lvhuahao@corp.netease.com>
2026-06-15 12:55:29 +0800

    feat: add AppConfig data models

---
+++ packages/core-kmp/src/commonMain/kotlin/com/dualler/core/model/AppConfig.kt
@@ -0,0 +1,40 @@
+package com.dualler.core.model
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Mini-program configuration
+ */
+@Serializable
+data class AppConfig(
+    val appId: String,
+    val appName: String,
+    val pages: List<String>,
+    val window: WindowConfig = WindowConfig(),
+    val tabBar: TabBarConfig? = null
+)
+
+@Serializable
+data class WindowConfig(
+    val backgroundColor: String = "#ffffff",
+    val navigationBarTitleText: String = "",
+    val navigationBarBackgroundColor: String = "#000000",
+    val navigationBarTextStyle: String = "white",
+    val enablePullDownRefresh: Boolean = false
+)
+
+@Serializable
+data class TabBarConfig(
+    val color: String,
+    val selectedColor: String,
+    val backgroundColor: String,
+    val list: List<TabBarItem>
+)
+
+@Serializable
+data class TabBarItem(
+    val pagePath: String,
+    val text: String,
+    val iconPath: String,
+    val selectedIconPath: String
+)
